9c9cbc00e61825a26559162827f66f941a040ebc
Katyana Cracknell <katyana.cracknell@gmail.com>
2013-11-16 16:42:50 +1100

    first commit

---
+++ app/views/comments/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @comment, :user_anme, :body, :idea_id, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Railsgirls::Application.config.secret_key_base = 'e6e981c36ec18dbd4b624fd3b3b0c229b4ddc31ca44fec092d15932ccdfc6b9a140233481afa5ae8be8bcb0c8ce8cbe0554219100388a620ef092d95fff8168b'
